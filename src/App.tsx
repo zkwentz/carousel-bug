7c29f5928ec6b29a1761496172c2e48c138850f9
Zach Wentz <zwentz@bluebeam.com>
2020-06-21 17:39:30 -0500

    simplified example

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Carousel from "@brainhubeu/react-carousel";
 import Component from './Component';
 import './App.css';
 import "@brainhubeu/react-carousel/lib/style.css";
 
 function App() {
-  const [properties] = useState(['a', 'b', 'c']);
-
   return (
     <div className="App">
-      <Carousel centered={true} slidesPerPage={2} dots>
-        {properties.map((property) => {
-          return <Component property={property} key={0} />;
+      <Carousel>
+        {['a','b','c'].map((property, index) => {
+          return <Component property={property} key={index} />;
         })}
       </Carousel>
     </div>
